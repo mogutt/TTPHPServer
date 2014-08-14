58a3090eac0cf669a057e2eee25ed4e568bd0f13
mingxin <mingxin@mogujie.com>
2014-08-14 18:29:16 +0800

    添加插件

--- TT/protected/views/group/add.php
+++ TT/protected/views/group/add.php
@@ -8,6 +8,7 @@
  * Desc: 
  */
 ?>
+<link rel="stylesheet" href="/css/bootstrap-multiselect.css" type="text/css"/>
 <style>
     .uploadclass{
         width:80px;
@@ -107,29 +108,21 @@
         ?>
     </button>
 </form>
-<!-- Button trigger modal -->
-<!--button class="btn btn-primary btn" data-toggle="modal" data-target="#myModal">
-    添加用户
-</button-->
-<!-- Modal>
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-            </div>
-            <div class="modal-body">
-                ...12121123123123
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-        </div>
-    </div>
-</div-->
+<select class="multiselect" multiple="multiple">
+    <option value="cheese">Cheese</option>
+    <option value="tomatoes">Tomatoes</option>
+    <option value="mozarella">Mozzarella</option>
+    <option value="mushrooms">Mushrooms</option>
+    <option value="pepperoni">Pepperoni</option>
+    <option value="onions">Onions</option>
+</select>
+<script type="text/javascript" src="js/bootstrap-multiselect.js"></script>
 
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.multiselect').multiselect();
+    });
+</script>
 <script type="text/javascript">
     $(document).ready(function(){
         $('#showtablediv').hide();
