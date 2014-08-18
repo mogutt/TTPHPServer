90a93739b2958bde408b87579fe5b02a4b8e93ef
mingxin <mingxin@mogujie.com>
2014-08-18 10:20:35 +0800

    修改text 长度

--- TT/protected/views/user/add.php
+++ TT/protected/views/user/add.php
@@ -19,23 +19,23 @@
     }
 </style>
 <form role="form" method="post" enctype="multipart/form-data">
-    <div class="form-group">
+    <div class="form-group col-xs-4">
         <label for="exampleInputEmail1">用户标题<span>(*)</span></label>
         <input type="text" class="form-control" id="exampleInputEmail1" placeholder="输入用户标题" name="data[title]" value="<?php $str = !empty($data->title) ? $data->title : ''; echo $str; ?>">
     </div>
-    <div class="form-group">
+    <div class="form-group col-xs-4">
         <label for="exampleInputPassword1">用户名<span>(*)</span></label>
         <input type="text" class="form-control" id="exampleInputPassword1" placeholder="输入用户名" name="data[uname]" value="<?php $str = !empty($data->uname) ? $data->uname : ''; echo $str; ?>">
     </div>
-    <div class="form-group">
+    <div class="form-group col-xs-4">
         <label for="exampleInputPassword1">花名<span>(*)</span></label>
         <input type="text" class="form-control" id="exampleInputPassword1" placeholder="输入花名" name="data[nickName]" value="<?php $str = !empty($data->nickName) ? $data->nickName : ''; echo $str; ?>">
     </div>
-    <div class="form-group">
+    <div class="form-group col-xs-4">
         <label for="exampleInputPassword1">密码<span>(*)</span></label>
         <input type="password" class="form-control" id="exampleInputPassword1" placeholder="输入密码" name="data[pwd]" value="<?php $str = !empty($data->pwd) ? $data->pwd : ''; echo $str; ?>">
     </div>
-    <div class="form-group">
+    <div class="form-group col-xs-4">
         <label for="exampleInputFile">头像</label>
         <input type="hidden" id="exampleInputFile" name="data[avatar]" value="<?php $str = !empty($data->avatar) ? $data->avatar : ''; echo $str; ?>">
         <input type="file" id="exampleInputFile" name="data[avatar]">
@@ -47,7 +47,7 @@
             }
         ?>
     </div>
-    <div class="form-group">
+    <div class="form-group col-xs-4">
         <label for="exampleInputPassword1">部门<span>(*)</span></label>
         <select id="depart"  name ="data[departId]">
             <?php
@@ -62,15 +62,15 @@
         </select>
     </div>
 
-    <div class="form-group">
+    <div class="form-group col-xs-4">
         <label for="exampleInputPassword1">手机号码<span>(*)</span></label>
         <input type="text" class="form-control" id="exampleInputPassword1" placeholder="输入手机号码" name="data[telphone]" value="<?php $str = !empty($data->telphone) ? $data->telphone : ''; echo $str; ?>">
     </div>
-    <div class="form-group">
+    <div class="form-group col-xs-4">
         <label for="exampleInputPassword1">邮箱<span>(*)</span></label>
         <input type="mail" class="form-control" id="exampleInputPassword1" placeholder="输入邮箱" name="data[mail]" value="<?php $str = !empty($data->mail) ? $data->mail : ''; echo $str; ?>">
     </div>
-    <div class="form-group">
+    <div class="form-group col-xs-4">
         <label for="exampleInputPassword1">性别<span>(*)</span></label>
         <div class="radio">
         <label>
@@ -81,15 +81,15 @@
         </label>
         </div>
     </div>
-    <div class="form-group">
+    <div class="form-group col-xs-4">
         <label for="exampleInputPassword1">地址<span>(*)</span></label>
         <input type="text" class="form-control" id="exampleInputPassword1" placeholder="输入地址" name="data[position]" value="<?php $str = !empty($data->position) ? $data->position : ''; echo $str; ?>">
     </div>
-    <div class="form-group">
+    <div class="form-group col-xs-4">
         <label for="exampleInputPassword1">工号<span>(*)</span></label>
         <input type="text" class="form-control" id="exampleInputPassword1" placeholder="输入工号" name="data[jobNumber]" value="<?php $str = !empty($data->jobNumber) ? $data->jobNumber : ''; echo $str; ?>">
     </div>
-    <div class="form-group">
+    <div class="form-group col-xs-4">
         <label for="exampleInputPassword1">状态<span>(*)</span></label>
         <div class="radio">
             <label>
